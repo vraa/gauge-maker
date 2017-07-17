35fd37048d8a9c2f6fc8266d97e96844ad28a302
Veera <veera.k.sundar@gmail.com>
2017-07-16 21:15:14 -0700

    Add source.

--- src/app-footer.js
+++ src/app-footer.js
@@ -5,7 +5,7 @@ class AppFooter extends Component {
     render() {
         return (
             <footer className="app-footer">
-                Made by <a href="https://veerasundar.com">Veera</a>
+                Made by <a href="https://veerasundar.com">Veera</a> |  <a href="https://github.com/vraa/gauge-maker">Source</a>
             </footer>
         )
     }
