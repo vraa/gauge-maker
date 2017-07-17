72e6e81320959d29812f79583c4af9f62b51f8c3
Veera <veera.k.sundar@gmail.com>
2017-07-16 18:15:04 -0700

    Download as SVG.

--- src/gauge-export.js
+++ src/gauge-export.js
@@ -1,11 +1,25 @@
-import React, {Component} from 'react';
+import React, {Component} from "react";
 import "./gauge-export.css";
 
 class GaugeExport extends Component {
+
+    encodeSVG = (svgString) => {
+        let uriPayload = svgString.replace(/"/g, "'");
+        return 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(uriPayload);
+    };
+
+    handleDownload = (e) => {
+        e.preventDefault();
+        let svgElm = document.querySelector(".gauge");
+        let svgXML = new XMLSerializer().serializeToString(svgElm);
+        window.open(this.encodeSVG(svgXML));
+
+    };
+
     render() {
         return (
             <div className="gauge-export">
-                <button>Download</button>
+                <button onClick={this.handleDownload}>Download</button>
             </div>
         )
     }
