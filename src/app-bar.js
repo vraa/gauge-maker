e7877593e74e3104db5945bd379ad798147f738a
Veera <veera.k.sundar@gmail.com>
2017-07-19 22:42:50 -0700

    Add tweet button.

--- src/app-bar.js
+++ src/app-bar.js
@@ -7,6 +7,10 @@ class AppBar extends Component {
         return (
             <header className="app-bar">
                 <h1>Gauge Generator</h1>
+                <p className="share"><a className="twitter-share-button"
+                      href="https://twitter.com/intent/tweet"
+                      data-size="large">
+                    Tweet</a></p>
             </header>
         )
     }
