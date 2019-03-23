d0f70a4f6fd3cb464e0d1b05fee064450fc9d316
Veera <veera.k.sundar@gmail.com>
2019-03-22 23:35:37 -0700

    Polish

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import "./carbon.css";
 import Gauge from "react-radial-gauge";
 import GaugeForm from "./gauge-form";
 import { themes } from "./themes";
@@ -46,15 +47,52 @@ class App extends Component {
     this.setState({ ...oldTheme, ...theme });
   };
 
+  handleGenerateClick = () => {
+    const svgString = new XMLSerializer().serializeToString(document.querySelector(".gauge"));
+    const uriPayload = svgString.replace(/"/g, "'");
+    this.setState({
+      svg: 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(uriPayload)
+    });
+  };
+
+  handleDone = () => {
+    this.setState({svg: null});
+  };
+
+  renderSVGDownload = () => {
+    const { svg } = this.state;
+    return <div className={'download'}>
+      <div>
+        <i>✔</i>
+        <p>Generated. Now, right click and select either 'Save As' or 'Open New Tab'.</p>
+        <a href={svg}>Right Click and Save Link As</a><br/>
+        <button onClick={this.handleDone}>Done</button>
+      </div>
+    </div>
+  };
+
   render() {
+    const { svg } = this.state;
     return (
       <main className="main">
         <div className="app">
-          <div className="gauge-display">
-            <Gauge
-              className="gauge"
-              {...this.state}
-            />
+          <div className={'showcase'}>
+            <header>
+              <h1>Gauge Generator</h1>
+              <p>Built by <a href={"https://veera.dev"}>Veera</a></p>
+            </header>
+            <div className="gauge-display">
+              <Gauge
+                className="gauge"
+                {...this.state}
+              />
+            </div>
+            <footer>
+              <button onClick={this.handleGenerateClick}>Generate SVG</button>
+            </footer>
+            <div className={'carbon'}>
+              <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?serve=CK7DLK7W&placement=veerasundarcom" id="_carbonads_js"></script>
+            </div>
           </div>
           <section className="content">
             <GaugeForm
@@ -63,6 +101,9 @@ class App extends Component {
               {...this.state}
             />
           </section>
+          {
+            svg ? this.renderSVGDownload() : null
+          }
         </div>
       </main>
     );
--- src/gauge-export.js
+++ src/gauge-export.js
@@ -1,80 +1,40 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./gauge-export.css";
 
 class GaugeExport extends Component {
 
-    constructor() {
-        super();
-        this.state = {
-            selected: null,
-            downloadContent: null
-        };
-    }
+  state = {
+    downloadContent: null
+  };
 
-    encodeSVG = (svgString) => {
-        let uriPayload = svgString.replace(/"/g, "'");
-        return 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(uriPayload);
-    };
+  encodeSVG = (svgString) => {
+    let uriPayload = svgString.replace(/"/g, "'");
+    return 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(uriPayload);
+  };
 
-    getGaugeContent = () => {
-        return new XMLSerializer().serializeToString(document.querySelector(".gauge"));
-    };
+  getGaugeContent = () => {
+    return new XMLSerializer().serializeToString(document.querySelector(".gauge"));
+  };
 
-    handleDownloadSVG = (e) => {
-        e.preventDefault();
-        this.setState({
-            selected: 'svg',
-            downloadContent: this.encodeSVG(this.getGaugeContent())
-        });
-    };
+  generateSVG = () => {
+    this.setState({
+      downloadContent: this.encodeSVG(this.getGaugeContent())
+    })
+  };
 
-    handleDownloadPNG = (e) => {
-        e.preventDefault();
-        let svgXML = this.getGaugeContent();
-        let imgElm = new Image();
-        imgElm.src = this.encodeSVG(svgXML);
-        let canvas = document.createElement('canvas');
-        canvas.width = this.props.size || 300;
-        canvas.height = this.props.size || 300;
-        imgElm.onload = () => {
-            canvas.getContext('2d').drawImage(imgElm, 0, 0);
-            let png = canvas.toDataURL("image/png");
-            
-            this.setState({
-                selected: 'png',
-                downloadContent: png
-            });
-        };
-    };
-
-    openContent = (e) => {
-        e.preventDefault();
-        let win = window.open();
-        win.document.write('<iframe src="' + this.state.downloadContent + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>')
-    };
-
-    render() {
-        let downloadElm = null;
-        console.log(this.state.downloadContent);
-        if (this.state.downloadContent) {
-            downloadElm = <button className="cta" onClick={this.openContent.bind(this)}>Download</button>;
+  render() {
+    const { downloadContent } = this.state;
+    return (
+      <div className="gauge-export">
+        {
+          downloadContent ? <div className={'instructions'}><p> <i>✓</i>Generated. Now, right Click and Select Save As or Open in New Tab</p> <a href={downloadContent}
+                                                                                             target={"_blank"}>Download</a>
+            </div> :
+            <button className={'cta'} onClick={this.generateSVG}>Generate SVG</button>
         }
-        return (
-            <div className="gauge-export">
-                <button
-                    className={this.state.selected === 'svg' ? 'selected' : ''}
-                    onClick={this.handleDownloadSVG.bind(this)}>SVG
-                </button>
-                <button
-                    className={this.state.selected === 'png' ? 'selected' : ''}
-                    onClick={this.handleDownloadPNG.bind(this)}>PNG
-                </button>
-                <p>
-                    {downloadElm}
-                </p>
-            </div>
-        )
-    }
+      </div>
+    )
+  }
 }
 
 export default GaugeExport;
