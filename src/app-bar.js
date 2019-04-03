56a3f4b592a7d7e42ca9cce05ad6d86f05f28c12
komv8i <phani.komaravolu@cchmc.org>
2019-04-03 14:42:58 -0400

    some more hooks refactoring

--- src/app-bar.js
+++ src/app-bar.js
@@ -1,14 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import "./app-bar.css";
 
-class AppBar extends Component {
-  render() {
-    return (
-      <header className="app-bar">
-        <h1>Gauge Generator</h1>
-      </header>
-    );
-  }
+const AppBar = () => {
+  return (
+    <header className="app-bar">
+      <h1>Gauge Generator</h1>
+    </header>
+  );
 }
 
 export default AppBar;
